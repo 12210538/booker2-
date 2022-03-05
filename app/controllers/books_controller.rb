28fde50aec4546abab5d58a9faa70404807c13dd
EC2 Default User <ec2-user@ip-172-31-44-113.ec2.internal>
2022-03-05 13:31:57 +0000

    課題提出

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,64 @@
+class BooksController < ApplicationController
+  before_action :correct_user, only: [:edit, :update]
+  def index
+    @book = Book.new
+    @books = Book.all
+    @user = User.find(current_user.id)
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+   
+    if @book.save
+      flash[:notice] = "You have created book successfully."
+      redirect_to book_path(@book.id)
+    else
+      @user =User.find(current_user.id)
+      @books = Book.all
+      render :index
+    end
+  end
+
+  def show
+    @book_new = Book.new
+    @book = Book.find(params[:id])
+    @user = User.find(@book.user_id)
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+    if @book.user_id != current_user.id
+      render book_path
+    end
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+       flash[:notice] = "You have update book successfully."
+       redirect_to book_path
+    else
+       render :edit
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+  def correct_user
+    @book = Book.find(params[:id])
+    @user = @book.user
+    redirect_to(books_path) unless @user == current_user
+  end
+
+end
